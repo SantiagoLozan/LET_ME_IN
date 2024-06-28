9a1561fced6977bdd1381de9ab8c7801790c403a
SantiagoLozan <81428336+SantiagoLozan@users.noreply.github.com>
2024-06-28 08:57:47 -0300

    botones, seguridad

---
+++ Assets/Scripts/s_BotonSeguridad.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s_BotonSeguridad : MonoBehaviour
+{
+    public GameObject PanelSeguridad;
+    private Coroutine toggleCoroutine;
+    public AudioSource audioSeguridad;
+
+    public void Peligro()
+    {
+        if (!PanelSeguridad.activeInHierarchy)
+        {
+            if (toggleCoroutine == null)
+            {
+                toggleCoroutine = StartCoroutine(TogglePanel());
+            }
+        }
+    }
+
+    IEnumerator TogglePanel()
+    {
+        while (true)
+        {
+            // Activate the panel and play sound
+            PanelSeguridad.SetActive(true);
+            audioSeguridad.Play();
+
+            // Wait for 2 seconds
+            yield return new WaitForSeconds(2f);
+
+            // Deactivate the panel
+            PanelSeguridad.SetActive(false);
+
+            // Wait for 2 seconds
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    public void DetenerPeligro()
+    {
+        if (toggleCoroutine != null)
+        {
+            StopCoroutine(toggleCoroutine);
+            toggleCoroutine = null;
+            PanelSeguridad.SetActive(false); // Optionally deactivate the panel when stopping
+            audioSeguridad.Stop(); // Stop the sound when deactivating the panel
+        }
+    }
+}
--- Assets/Scripts/s_InformacionVisible.cs
+++ Assets/Scripts/s_InformacionVisible.cs
@@ -17,6 +17,6 @@ public class s_InformacionVisible : MonoBehaviour
         {
             PanelInformacion.SetActive(false);
         }
+
     }
 }
-
