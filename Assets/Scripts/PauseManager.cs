2bc1bcd02609723750f705a663718a771ed5e139
valentina-rojas <valentinarojas618@gmail.com>
2024-09-11 15:06:07 -0300

    fix pausa

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -61,17 +61,18 @@ public class PauseManager : MonoBehaviour
         isPaused = true;
     }
 
-  /*  public void RestartGame()
+   /* public void RestartGame()
     {
         Time.timeScale = 1f;           // Restablece el tiempo normal
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);  // Reinicia la escena actual
-    }
-
-    public void ReturnToMenu()
+        pauseMenuUI.SetActive(false);  // Asegúrate de que el menú de pausa esté desactivado
+    }*/
+   public void ReturnToMenu()
     {
         Time.timeScale = 1f;           // Restablece el tiempo normal
         SceneManager.LoadScene("MenuPrincipal");  // Carga la escena del menú principal
-    }*/
+        pauseMenuUI.SetActive(false);  // Asegúrate de que el menú de pausa esté desactivado
+    }
 }
 
 
